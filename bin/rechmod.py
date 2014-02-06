ee466e3a78c1b2983c841a4baeb9da360db6b9ea
Charles Doutriaux <doutriaux1@llnl.gov>
2014-02-05 20:20:15 -0500

    fix for weird stat with length > 4

--- bin/rechmod.py
+++ bin/rechmod.py
@@ -12,8 +12,12 @@ for f in files:
        continue
     md=s.st_mode
     m=oct(stat.S_IMODE(md))
+    if len(m)>4:
+      print "Skipping:",m
+      continue
     m2=m[:2]+m[1]+m[1]
     print m,m2
+    continue
     try:
       os.chmod(fnm,eval(m2))
     except:
