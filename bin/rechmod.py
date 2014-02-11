d155210a8429429373c403833a5be6e6849ceab7
Charles Doutriaux <doutriaux1@llnl.gov>
2014-02-11 16:57:30 -0500

    working better rechmod

--- bin/rechmod.py
+++ bin/rechmod.py
@@ -12,16 +12,18 @@ for f in files:
        continue
     md=s.st_mode
     m=oct(stat.S_IMODE(md))
-    if len(m)>4:
-      print "Skipping:",m
+    if len(m)==5 and m[1]=='2':
+      m2 = m[0]+m[2]+m[1]+m[1]
+    elif len(m)==4:
+      m2=m[:2]+m[1]+m[1]
+    else:
+      print "Skipping:",m,len(m),m[1]
       continue
-    m2=m[:2]+m[1]+m[1]
-    print m,m2
-    continue
+    #print m,m2
     try:
       os.chmod(fnm,eval(m2))
     except:
       print "\t Failed for: ",fnm    
     if stat.S_ISDIR(md):
-        print "Going into:",fnm
+        print "Going into:",fnm,"went from",m,"to",m2
 
