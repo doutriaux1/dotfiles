ba1c3e480edd7b817ab257e094610e8eebe6924c
Charles Doutriaux <doutriaux1@llnl.gov>
2016-06-29 11:49:24 -0700

    exec to clean cmake generated conda envs

---
+++ bin/clean_conda.py
@@ -0,0 +1,15 @@
+#/usr/bin/env python
+import subprocess
+import shlex
+
+def call(cmd):
+    cmd = shlex.split(cmd)
+    p = subprocess.Popen(cmd,stdout=subprocess.PIPE,stdin=subprocess.PIPE,stderr=subprocess.PIPE)
+    p.wait()
+    return p.stdout.read()
+envs = call("conda env list")
+for e in envs.split("\n"):
+    if e!="" and e[0]=="v" and e.find("-g")>-1:
+        print "cleaning:",e
+        call("conda remove -y --all -n %s" % e.split()[0])
+
